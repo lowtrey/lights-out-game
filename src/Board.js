7a70ce4a4fb95f6032c6c202e128662ab05b2e71
Q. Price <priceqp@gmail.com>
2019-06-24 18:00:19 -0500

    Add neon styling to game

--- src/Board.js
+++ src/Board.js
@@ -96,6 +96,16 @@ class Board extends Component {
   /** Render game board or winning message. */
 
   render() {
+    if(this.state.hasWon) {
+      return (
+        <div className='Board-title'>
+          <div className='winner'>
+            <span className='neon-orange'>YOU</span>
+            <span className='neon-blue'>WIN!</span>
+          </div>
+        </div>
+      )
+    }
     let tblBoard = [];
     for (let y = 0; y < this.props.nrows; y++){
       let row = [];
@@ -111,14 +121,19 @@ class Board extends Component {
       }
       tblBoard.push(<tr key={y}>{row}</tr>);
     }
-    let gameState = this.state.hasWon
-      ? <h1>You Win!!</h1> 
-      : (<table className='Board'>
-            <tbody>
-              {tblBoard}
-            </tbody>
-          </table>);
-    return gameState;
+    return(
+      <div>
+        <div className='Board-title'>
+          <div className='neon-orange'>Lights</div>
+          <div className='neon-blue'>Out</div>
+        </div>
+        <table className='Board'>
+          <tbody>
+            {tblBoard}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
